a0c9e92acf4986a4c2dce7d1efc46f0a0f9a38ca
GKava <1devstack1@gmail.com>
2018-10-10 15:40:44 +0300

    sss

--- app/src/main/java/bar/appbarbottom/appbarbottom/MainActivity.java
+++ app/src/main/java/bar/appbarbottom/appbarbottom/MainActivity.java
@@ -1,9 +1,14 @@
 package bar.appbarbottom.appbarbottom;
 
+import android.support.design.bottomappbar.BottomAppBar;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -24,6 +29,16 @@ private static FragmentManager fragmentManager;
             }
             fragmentManager.beginTransaction().add(R.id.fragment_container, new MainFragment()).commit();
         }
+
+        BottomAppBar bar = (BottomAppBar) findViewById(R.id.app_bar);
+
+
+        bar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Handle the navigation click by showing a BottomDrawer etc.
+            }
+        });
     }
 
     @Override
@@ -39,5 +54,29 @@ switch (view.getId()){
 //            .addToBackStack(null)
 //            .commit();
 }
+    }
+
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        int id = item.getItemId();
+//        switch (id) {
+//            case R.id.app_bar_fav:
+//
+//                return true;
+//            case R.id.app_bar_search:
+//
+//                return true;
+//            case R.id.app_bar_settings:
+//
+//                return true;
+//            default:
+//                return super.onOptionsItemSelected(item);
+//        }
+//    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.bottomappbar_menu, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
